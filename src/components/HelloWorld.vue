b60baa03f7e895b76bec3e1724651569bd00c6fe
ntropish <ntropish@gmail.com>
2019-09-26 20:10:18 -0500

    Install vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,161 +1,142 @@
 <template>
-  <div id="app" @mousewheel="onScroll">
-    <h2>Intra</h2>
-    <frame :key="id" :id="id" v-for="id in frameStack" />
-  </div>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Vuetify
+        </h1>
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+        </p>
+      </v-flex>
+
+      <v-flex
+        mb-5
+        xs12
+      >
+        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-layout>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(link, i) in importantLinks"
+            :key="i"
+            :href="link.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-layout>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(eco, i) in ecosystem"
+            :key="i"
+            :href="eco.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ eco.text }}
+          </a>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import * as ft from "froto";
-import Vue from "vue";
-import Frame from "./components/Frame";
 export default {
-  name: "app",
-  components: {
-    Frame
-  },
-  data() {
-    return {
-      // [ [xRange], [yRange] ]
-      view: [[-21, 21], [-21, 21]],
-      frames: {
-        // actual implementation will need uuids, maybe urls too
-        0: {
-          name: "root",
-          parent: null,
-          children: [1],
-          bounds: [[-20, 20], [-20, 20]],
-          hue: 280
-          // optional frame properties:
-          // type: null // default/null type is just a basic frame
-          // tags: [] // searchable strings for users to locate frames
-          // data: {} // storage for details about this node
-        },
-        1: {
-          name: "right",
-          parent: 0,
-          children: [2],
-          bounds: [[-17, 17], [-17, 17]],
-          hue: 80
-        },
-        2: {
-          name: "corner",
-          parent: 1,
-          children: [],
-          bounds: [[-16, 0], [-16, 0]],
-          hue: 120
-        }
+  data: () => ({
+    ecosystem: [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader',
       },
-      nextFrameId: 1,
-      rootFrame: 0
-    };
-  },
-  methods: {
-    onScroll(e) {
-      const change = (0.05 * e.deltaY) / 100;
-      const currentDurationX = ft.duration(this.view[0]);
-      const xOrigin = ft.from(this.screen[0], e.clientX);
-      const yOrigin = ft.from(this.screen[1], e.clientY);
-      const currentDurationY = ft.duration(this.view[1]);
-
-      const newXRange = ft.grow(
-        currentDurationX * change,
-        xOrigin,
-        this.view[0]
-      );
-      Vue.set(this.view, 0, newXRange);
-
-      const newYRange = ft.grow(
-        currentDurationY * change,
-        yOrigin,
-        this.view[1]
-      );
-      Vue.set(this.view, 1, newYRange);
-    },
-    frameStyle(id) {
-      const frame = this.frames[id];
-      const top = ft.from(this.view[1], frame.bounds[1][0]);
-      const width = ft.duration(frame.bounds[0].map(ft.from(this.view[0])));
-      const left = ft.from(this.view[0], frame.bounds[0][0]);
-      const height = ft.duration(frame.bounds[1].map(ft.from(this.view[1])));
-      const background = `hsla(${frame.hue}, 70%, 60%, 0.9)`;
-      // 0 1px 1em hsla(0, 0%, 0%, 0.2)
-      const boxShadow =
-        `0 1em 1em -0.5em hsla(${frame.hue}, 80%, 20%, 0.2),` +
-        `0 2em 2em -1em hsla(${frame.hue}, 80%, 20%, 0.3)`;
-      return {
-        boxShadow,
-        background,
-        top: top * 100 + "%",
-        left: left * 100 + "%",
-        width: width * 100 + "%",
-        height: height * 100 + "%"
-      };
-    }
-  },
-  computed: {
-    frameStack() {
-      let result = [];
-      const scanFrame = id => {
-        const frame = this.frames[id];
-        const containsX = ft.containsRange(this.view[0], frame.bounds[0]);
-        const containsY = ft.containsRange(this.view[1], frame.bounds[1]);
-        if (containsX && containsY) {
-          result.push(id);
-          frame.children.forEach(scanFrame);
-        }
-      };
-      scanFrame(this.rootFrame);
-      return result;
-    },
-    screen() {
-      return [[0, this.$el.scrollWidth], [0, this.$el.scrollHeight]];
-    }
-  }
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify',
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify',
+      },
+    ],
+    importantLinks: [
+      {
+        text: 'Documentation',
+        href: 'https://vuetifyjs.com',
+      },
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com',
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuejs.com/vuetify',
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs',
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify',
+      },
+    ],
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com/components/api-explorer',
+      },
+      {
+        text: 'Select a layout',
+        href: 'https://vuetifyjs.com/layout/pre-defined',
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+      },
+    ],
+  }),
 };
 </script>
-
-<style>
-@import url("https://fonts.googleapis.com/css?family=Rajdhani&display=swap");
-html,
-body {
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-body {
-  background: #1c1e20;
-}
-#app {
-  overflow: hidden;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-  font-size: 1.4em;
-  font-family: "Rajdhani", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #a598bd;
-  font-weight: 200;
-}
-.frame {
-  font-weight: 900;
-  font-size: 3em;
-  text-align: left;
-  border-radius: 2px;
-  position: absolute;
-}
-
-.corner-title {
-  line-height: 0.8em;
-  margin: 4px;
-  color: hsla(0, 0%, 100%, 0.7);
-  text-shadow: 0px 2px 0.3em hsla(0, 0%, 100%, 0.2);
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import './plugins/fontawesome'
 import App from './App.vue'
 import { VueHammer } from 'vue2-hammer'
+import vuetify from './plugins/vuetify';
 
 Vue.use(VueHammer)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
